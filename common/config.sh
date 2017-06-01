73e151ea4448f25126b53217fb4940b0808e8e4c
Bernhard Waldbrunner <vbwx01@gmail.com>
2017-06-01 13:55:39 +0200

    Remove unnecessary declarations

--- common/config.sh
+++ common/config.sh
@@ -1,8 +1,8 @@
 prefix=/usr/local
 cfg="$HOME/.config/dotfiles"
 
-export TMPDIR="${TMPDIR:-/tmp}"
-export RBENV_ROOT="$prefix/rbenv"
-export PYENV_ROOT="$prefix/pyenv"
-export NVM_DIR="$prefix/nvm"
-export PERLBREW_ROOT="$prefix/perlbrew"
+TMPDIR="${TMPDIR:-/tmp}"
+RBENV_ROOT="$prefix/rbenv"
+PYENV_ROOT="$prefix/pyenv"
+NVM_DIR="$prefix/nvm"
+PERLBREW_ROOT="$prefix/perlbrew"
