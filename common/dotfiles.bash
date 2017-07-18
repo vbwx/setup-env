25f227aebb26a320eec454f65b251b10904963ac
Bernhard Waldbrunner <vbwx01@gmail.com>
2017-07-18 15:10:29 +0100

    Add existence check for dotfiles

--- common/dotfiles.bash
+++ common/dotfiles.bash
@@ -1,2 +1,4 @@
-clone https://github.com/vbwx/dotfiles $HOME/.config/dotfiles
-$HOME/.config/dotfiles/install
+if ! inhome .config/dotfiles; then
+	clone https://github.com/vbwx/dotfiles $HOME/.config/dotfiles
+	$HOME/.config/dotfiles/install
+fi
