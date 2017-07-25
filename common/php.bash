ad1d114e81223dd816fe1793908cb7fc1ce3928a
Bernhard Waldbrunner <vbwx01@gmail.com>
2017-07-25 16:25:10 +0100

    Add phpenv setup

--- common/php.bash
+++ common/php.bash
@@ -1,5 +1,20 @@
-if ! exist composer; then
-	download "https://getcomposer.org/installer" "$TMPDIR/composer.php"
-	rundel php --install-dir=$prefix/bin --filename=composer "$TMPDIR/composer.php"
+# Variables:
+#     php_ver (string) The PHP version to be installed
+
+local ver
+
+clone https://github.com/phpenv/phpenv "$prefix/phpenv"
+exist phpenv || PATH="$prefix/phpenv/bin:$PATH"
+
+islocal php || eval "$(phpenv init -)"
+
+if ! islocal php; then
+	ver="$(myvar ver latest)"
+	[[ $ver == "latest" ]] && ver="$(phpenv install --releases | sort -nr | perl -ne \
+		'next unless /^\s*php-\d+\.\d+(\.\d+)?\n$/; s/\s//g; print; exit;')"
+	phpenv install "$ver"
+	phpenv global "$ver"
+	phpenv rehash
 fi
 
+islocal php || rerun
